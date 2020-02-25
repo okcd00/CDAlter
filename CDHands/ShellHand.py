6274f25c91bd0731623fa1e89ac2bb479b6a1ddc
Dian Chen <okcd00@qq.com>
2020-02-25 19:58:12 +0800

    Update: allow ShellHand to operate on simple shell scripts.

--- CDHands/ShellHand.py
+++ CDHands/ShellHand.py
@@ -7,23 +7,43 @@
 #   date     : 2020-02-20
 #   desc     :
 # ==========================================================================
+import os
 import win32api
 import win32con
+
 
 class ShellHand(object):
     def __init__(self, ):
         self.current_status = -1
-        self.current_position = (-1, -1)
 
-    def open(self, file_path):
+    def open_with(self, file_path, program_path=None):
         self.current_status = win32api.ShellExecute(
             1,  # handle for father window
             'open',  # action in {"edit", "explore", "open", "find", "print", "NULL"}
-            r'{}'.format(file_path),  # file_name
-            '',  # parameters, such as '-perfectworld'
+            r'{}'.format(program_path),  # file_name
+            r'{}'.format(file_path),  # parameters, such as '-perfectworld'
             '',  # default dir
             win32con.SW_SHOWNORMAL  # show command in {}
         )
+        return self.current_status
+
+    def open(self, program_path):
+        self.current_status = self.open_with(
+            file_path='',
+            program_path=program_path)
+        return self.current_status
+
+    def exec(self, text):
+        if not isinstance(text, list):
+            if os.path.exists(text):
+                file_path = text
+                text = [line.strip() for line in open(file_path, 'r')]
+            else:
+                text = [line.strip() for line in text.split()]
+        text = [line.strip() for line in text if not line.startswith('#')]
+        for line in text:
+            os.system(line)
+        return text
 
     def show_status(self):
         status_dict = {
@@ -41,7 +61,8 @@ class ShellHand(object):
         return status_dict.get(self.current_status, 'Õý³£')
 
     def __repr__(self):
-        pass
+        _st = self.show_status()
+        return u'<CD ShellHand Instance> with status [{}]'.format(_st)
 
     def __str__(self):
         pass
@@ -50,3 +71,4 @@ class ShellHand(object):
 if __name__ == '__main__':
     sh = ShellHand()
     sh.open('C:\\ProgramData\\Anaconda3\\python.exe')
+    sh.open_with('README.md', 'notepad++.exe')
---
+++ CDMemory/add_conda_source.sh
@@ -0,0 +1,10 @@
+# 添加中科大的conda源
+conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/main/
+conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/conda-forge/
+conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/msys2/
+conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/bioconda/
+conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/menpo/
+
+# 设置搜索时显示通道地址
+conda config --set show_channel_urls yes
