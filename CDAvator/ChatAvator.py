f2b3c3c25403bf3285313353ca03113e51db2b45
Dian Chen <okcd00@qq.com>
2023-03-25 23:29:35 +0800

    Update: ChatAvator passes tests.

--- CDAvator/ChatAvator.py
+++ CDAvator/ChatAvator.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 
 openai_key = "sk-<YOUR_KEY>"
 openai_chat_api = "https://api.openai.com/v1/chat/completions"
-proxy = 'socks5://127.0.0.1:1087'
+proxy = 'socks5://127.0.0.1:1081'
 # proxy = 'socks5://100.64.0.15:11081'
 # proxy = 'socks5://100.64.0.42:7890'
 
@@ -21,24 +21,29 @@ headers = {
 histories = defaultdict(list)
 
 
-def get_reply(messages):
+def get_reply(prompt, history=None):
     transport = SyncProxyTransport.from_url(proxy)
     try:
+        if history is None:
+            history = []
+        history.append({"role": "user", "content": prompt})
         with httpx.Client(transport=transport) as client:
             resp = client.post(
-                openai_chat_api, json={
-                    "model": "gpt-3.5-turbo", "messages": messages}, 
-                    headers=headers,
-                timeout=5 * 60)
+                openai_chat_api, 
+                json={"model": "gpt-3.5-turbo", "messages": history}, 
+                headers=headers, timeout=5 * 60)
             data = resp.json()
             if data.get('choices'):
                 reply = data['choices'][0]['message']
+                history.append(reply)
                 return reply['content']
+            else:
+                raise Exception("No Response. \n{}".format(data))
     except Exception as e:
         import logging
         logging.exception(e)
         return "请求失败，请重试"
-
+        
 
 def test():
     text = """
@@ -61,5 +66,65 @@ def test():
     print(result)
 
 
+def input_with_enter(hint=''):
+    """遇到一个空行返回结果"""
+    text = []
+    pre_t = input(hint)
+    while pre_t != '':
+        text.append(pre_t)
+        pre_t = input()
+    print('<Jarvis: received>')
+    return '\n'.join(text)
+
+
+def save_history(history, save_path):
+    """json line"""
+    import json
+    import time 
+    with open(save_path, 'a') as f:
+        f.write(time.strftime('%Y/%m/%d-%H:%M:%S '))
+        json.dump(history, f)
+        f.write('\n')
+
+
+def save_and_clear_history(history, instruct='', save_path='./logs/chat_history.log'):
+    save_history(history, save_path)
+    history = [{"role": "system", "content": instruct}]
+    print("已经清除历史记录，请重新提问。")
+    return history
+
+
+def start_chat(instruct, multiline_input=True):
+    history = [{"role": "system", "content": instruct}]
+    try:
+        while True:
+            print('-'*40+'\n')
+            if multiline_input:
+                question = input_with_enter("问：")
+            else:
+                question = input('问：')
+            if question == 'clear':
+                history = save_and_clear_history(history, instruct)
+                continue
+            rep = get_reply(question, history)
+            print("\nJarvis:\n", rep)
+    except Exception as e:
+        save_and_clear_history(history)
+        raise e 
+
+
+def main():
+    import sys
+    instruct = "你是一个叫作Jarvis的专业工作助手，需要回答各种具备专业知识的问题，要求回答严谨、精确。"
+    if len(sys.argv) > 1:
+        instruct = sys.argv[1]
+    start_chat(instruct=instruct)
+
+
 if __name__ == "__main__":
-    pass
+    # pip install -r requirements.txt
+    """
+    export OPENAI_PROXY=127.0.0.1:1081
+    OPENAI_KEY=sk-<YOUR_KEY> python start_chat.py "You are my friend and you know my name is Kari."
+    """
+    main()
